0e42050e9164db1a614456e889e9e69d0b3858cb
beim <805908499@qq.com>
2018-11-18 19:18:50 +0800

    11

--- transeTf.py
+++ transeTf.py
@@ -31,40 +31,50 @@ class TransE_tf():
         logger.info('init transe, config: %s' % json.dumps(config))
 
     def train(self):
-        entity_embedding_table = tf.Variable(
-            tf.truncated_normal([self.entity_size, self.dim], stddev=6.0 / math.sqrt(self.dim)))
-        # entity_embedding_table = entity_embedding_table / tf.norm(entity_embedding_table, axis=1, keepdims=True)
-        relation_embedding_table = tf.Variable(
-            tf.truncated_normal([self.relation_size, self.dim], stddev=6.0 / math.sqrt(self.dim)))
-        # relation_embedding_table = relation_embedding_table / tf.norm(relation_embedding_table, axis=1, keepdims=True)
+        with tf.name_scope("embedding"):
+            entity_embedding_table = tf.Variable(
+                tf.truncated_normal([self.entity_size, self.dim], stddev=6.0 / math.sqrt(self.dim)))
+            # entity_embedding_table = entity_embedding_table / tf.norm(entity_embedding_table, axis=1, keepdims=True)
+            relation_embedding_table = tf.Variable(
+                tf.truncated_normal([self.relation_size, self.dim], stddev=6.0 / math.sqrt(self.dim)))
+            # relation_embedding_table = relation_embedding_table / tf.norm(relation_embedding_table, axis=1, keepdims=True)
 
-        triples = tf.placeholder(tf.int32, shape=[self.batch_size, 3])
-        corrupted_triples = tf.placeholder(tf.int32, shape=[self.batch_size, 3])
+            triples = tf.placeholder(tf.int32, shape=[self.batch_size, 3], name="triples")
+            corrupted_triples = tf.placeholder(tf.int32, shape=[self.batch_size, 3], name="corrupted_triples")
 
-        head_embedding = tf.nn.embedding_lookup(entity_embedding_table, triples[:, 0])
-        tail_embedding = tf.nn.embedding_lookup(entity_embedding_table, triples[:, 2])
-        relation_embedding = tf.nn.embedding_lookup(relation_embedding_table, triples[:, 1])
-        corrupted_head_embedding = tf.nn.embedding_lookup(entity_embedding_table, corrupted_triples[:, 0])
-        corrupted_tail_embedding = tf.nn.embedding_lookup(entity_embedding_table, corrupted_triples[:, 2])
+        with tf.name_scope("lookup"):
+            head_embedding = tf.nn.embedding_lookup(entity_embedding_table, triples[:, 0])
+            tail_embedding = tf.nn.embedding_lookup(entity_embedding_table, triples[:, 2])
+            relation_embedding = tf.nn.embedding_lookup(relation_embedding_table, triples[:, 1])
+            corrupted_head_embedding = tf.nn.embedding_lookup(entity_embedding_table, corrupted_triples[:, 0])
+            corrupted_tail_embedding = tf.nn.embedding_lookup(entity_embedding_table, corrupted_triples[:, 2])
 
-        loss = self.calc_loss(head_embedding, relation_embedding, tail_embedding, corrupted_head_embedding, corrupted_tail_embedding)
-        global_step = tf.placeholder(dtype=tf.int32)
-        learning_rate = tf.train.exponential_decay(self.learning_rate, global_step, self.decay_steps, self.decay_rate, staircase=True)
-        optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
+        with tf.name_scope("loss"):
+            loss = self.calc_loss(head_embedding, relation_embedding, tail_embedding, corrupted_head_embedding, corrupted_tail_embedding)
+            global_step = tf.placeholder(dtype=tf.int32)
+            learning_rate = tf.train.exponential_decay(self.learning_rate, global_step, self.decay_steps, self.decay_rate, staircase=True)
+            # optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
-        new_entity_embedding_table = entity_embedding_table / tf.norm(entity_embedding_table, axis=1, keepdims=True)
-        update_entity_embedding_table = tf.assign(entity_embedding_table, new_entity_embedding_table)
-        new_relation_embedding_table = relation_embedding_table / tf.norm(relation_embedding_table, axis=1, keepdims=True)
-        update_relation_embedding_table = tf.assign(relation_embedding_table, new_relation_embedding_table)
+            new_entity_embedding_table = entity_embedding_table / tf.norm(entity_embedding_table, axis=1, keepdims=True)
+            update_entity_embedding_table = tf.assign(entity_embedding_table, new_entity_embedding_table)
+            new_relation_embedding_table = relation_embedding_table / tf.norm(relation_embedding_table, axis=1, keepdims=True)
+            update_relation_embedding_table = tf.assign(relation_embedding_table, new_relation_embedding_table)
+
+            tf.summary.scalar(name=loss.op.name, tensor=loss)
+            merge = tf.summary.merge_all()
 
         init = tf.global_variables_initializer()
         with tf.Session(config=tf.ConfigProto(log_device_placement=False)) as sess:
             sess.run(init)
+            summary_writer = tf.summary.FileWriter(logdir="./transE_all_sumary_normal/", graph=sess.graph)
             sess.run(update_relation_embedding_table)
             for step in range(self.max_epoch):
                 triple_ids, corrupted_triple_ids = self.next_batch(self.batch_size)
                 sess.run(update_entity_embedding_table)
-                r, _ = sess.run([loss, optimizer], feed_dict={triples: triple_ids, corrupted_triples: corrupted_triple_ids, global_step: step})
+                r, _, summary = sess.run([loss, optimizer, merge], feed_dict={triples: triple_ids, corrupted_triples: corrupted_triple_ids, global_step: step})
+                if step % 50 == 0:
+                    summary_writer.add_summary(summary, step)
                 if step % 1000 == 0:
                     print('step %d, loss = %s, %s' % (step, r, time.asctime()))
                     logger.info('step %d, loss = %s' % (step, r))
@@ -75,6 +85,7 @@ class TransE_tf():
                     logger.info('step %d, loss = %s' % (step, r))
                     self.snapshot(step, entity_embedding_table, relation_embedding_table)
                     break
+
     def snapshot(self, step, entity_embedding_table, relation_embedding_table):
         np.savetxt('%s/snapshot/entity_embedding_table_%s.txt' % (currdir, str(step)), entity_embedding_table.eval())
         np.savetxt('%s/snapshot/relation_embedding_table_%s.txt' % (currdir, str(step)), relation_embedding_table.eval())
